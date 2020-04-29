00e1203e22ca14658bcd6a9aef5dda3bf1f3d036
James Santucci <james.santucci@gmail.com>
2020-04-29 15:24:21 -0500

    Open up Franklin server to us east ips (#4)
    * Add us east ips to alb
    
    * Poke firewall hole for us east 1 ips

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -23,14 +23,45 @@ resource "aws_security_group_rule" "alb_https_ingress" {
   security_group_id = aws_security_group.alb.id
 }
 
-resource "aws_security_group_rule" "alb_container_instance_egress" {
-  type      = "egress"
-  from_port = 0
-  to_port   = 65535
-  protocol  = "tcp"
+# US-East Whitelist
 
-  security_group_id        = aws_security_group.alb.id
-  source_security_group_id = data.terraform_remote_state.core.outputs.container_instance_security_group_id
+data "aws_ip_ranges" "us_east_ec2" {
+  regions  = ["${var.aws_region}"]
+  services = ["ec2"]
+}
+
+locals {
+  ec2_cidr_block_chunks = "${chunklist(data.aws_ip_ranges.us_east_ec2.cidr_blocks, 40)}"
+}
+
+resource "aws_security_group" "franklin_server_alb_whitelist_ec2" {
+  count = "${length(local.ec2_cidr_block_chunks)}"
+
+  vpc_id = data.terraform_remote_state.core.outputs.vpc_id
+
+  egress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    security_groups = [data.terraform_remote_state.core.outputs.container_instance_security_group_id]
+  }
+
+  tags = {
+    Name        = "sgFranklinServerLoadBalancer"
+    Project     = "${var.project}"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_security_group_rule" "alb_franklin_server_ec2_https_ingress" {
+  count = "${length(local.ec2_cidr_block_chunks)}"
+
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = local.ec2_cidr_block_chunks[count.index]
+  security_group_id = aws_security_group.franklin_server_alb_whitelist_ec2.*.id[count.index]
 }
 
 #
